f4d04cde2f772839ba8d54c1dd8bf5d035c0fed4
onealtang <oneal.tang@gmail.com>
2015-07-15 01:19:21 +0800

    get data in request and set into bd.BidRequest

--- engine/BDInvoker.go
+++ engine/BDInvoker.go
@@ -15,7 +15,20 @@ import (
 	"github.com/golang/protobuf/proto"
 	"time"
 	"github.com/franela/goreq"
+	"strings"
+	"adexchange/lib"
+	m "adexchange/models"
 )
+
+var (
+	OS_MAP = map[int]bd.Device_Os{
+		0: bd.Device_ANDROID,
+		1: bd.Device_IOS,
+		2: bd.Device_IOS,
+		3: bd.Device_IOS,
+	}
+)
+
 
 func invokeBD2(demand *Demand) {
 
@@ -48,53 +61,66 @@ func invokeBD2(demand *Demand) {
 
 func invokeBD(demand *Demand) {
 
-	beego.Debug("target url: ", demand.URL)
-
-
+	// current baidu api version is 4.0
+	// TODO move this to conf file
 	apiVersion := &bd.Version{
 		Major: pUint32(4),
 		Minor: pUint32(0),
 	}
-	appVersion := &bd.Version{
-		Major: pUint32(4),
-		Minor: pUint32(0),
-	}
+//	appVersion := &bd.Version{
+//		Major: pUint32(4),
+//		Minor: pUint32(0),
+//	}
 
+	/*  App   (required)*/
 
-	var appId string = "c5702dff";			// required
-	var appBundleId string = "com.xxxxx"	// required
-	var appName string = "";
-	userPermissionType := bd.App_UserPermission_ACCESS_FINE_LOCATION
-	userPermissionStatus := bd.App_UserPermission_UNKNOWN
-	appCategories := []uint32{uint32(1)}			// required
+	// TODO put appid in secret_key 字段???
+	var appId string = demand.AdSecretKey;			// required
+
+	// below are optional
+//	var appBundleId string = "com.xxxxx"	// required
+//	var appName string = "";
+//	appCategories := []uint32{uint32(1)}			// required
+//	userPermissionType := bd.App_UserPermission_ACCESS_FINE_LOCATION
+//	userPermissionStatus := bd.App_UserPermission_UNKNOWN
 	app := &bd.App{
 		Id: &appId,
-		StaticInfo: &bd.App_StaticInfo{
-			BundleId: &appBundleId,
-			Name: &appName,
-			Categories: appCategories,
-		},
-		Version: appVersion,
-		UserPermission: []*bd.App_UserPermission{
-			&bd.App_UserPermission{
-				Type: &userPermissionType,		// required
-				Status: &userPermissionStatus,					// required
-			},
-		},
+
+		// optional.  because there's no categories, so don't provide static info
+//		StaticInfo: &bd.App_StaticInfo{
+//			BundleId: &appBundleId,		// required
+//			Name: &appName,
+//			Categories: appCategories,	// required
+//		},
+//		Version: appVersion,
+//		UserPermission: []*bd.App_UserPermission{
+//			&bd.App_UserPermission{
+//				Type: &userPermissionType,		// required
+//				Status: &userPermissionStatus,					// required
+//			},
+//		},
 	}
 
+
+	/*  Device  (required)*/
+	stringArr := strings.Split(demand.AdRequest.Osv, ".")
+
 	devOsVersion := &bd.Version{
-		Major: pUint32(8),
-		Minor: pUint32(4),
+		Major: pUint32(lib.ConvertStrToInt(stringArr[0])),
+		Minor: pUint32(lib.ConvertStrToInt(stringArr[1])),
 	}
-	devModel := "Iphone6"
-	devVendor := "China MObile"
-	devId := "123abc"
-	devUdid := bd.Device_UdId{
-		Idfa: &devId,
-	}
+	devModel := demand.AdRequest.Device		// IPhone5s
+	var devVendor string					// Apple
+	if demand.AdRequest.Os == 1 {devVendor = "Apple"} else { devVendor = "Google"}
+
+	devUdid := bd.Device_UdId{}
+
+	if demand.AdRequest.Idfa != "" { devUdid.Idfa = &demand.AdRequest.Idfa }
+	if demand.AdRequest.Imei != "" { devUdid.Imei = &demand.AdRequest.Imei }
+	if demand.AdRequest.Wma != "" { devUdid.Mac = &demand.AdRequest.Wma }
+
 	devType := bd.Device_PHONE
-	devOs := bd.Device_IOS
+	devOs := OS_MAP[demand.AdRequest.Os]
 	dev := &bd.Device{
 		Type: &devType,					// required. Mobile, Tablet, TV
 		Os: &devOs,						// required. android or IOS
@@ -104,26 +130,28 @@ func invokeBD(demand *Demand) {
 		Udid: &devUdid,					// required. ios: idfa, mac, android: imei, mac, tv: imei, mac, idfv
 	}
 
+	/*  Network  (required) */
 	nt := &bd.Network{
-
+		Ipv4: &demand.AdRequest.Ip,
 	}
 
-	adSpaceId := "L000015a"
-	adWidth := 600
-	adHeight := 400
+
+	/* Adslot  (required) */
+	adSpaceId := demand.AdspaceKey
+	adWidth := demand.AdRequest.Width
+	adHeight := demand.AdRequest.Height
 	adSize := bd.Size{
-		Width: pUint32(adWidth),		// required
-		Height: pUint32(adHeight),		// required
+		Width: pUint32(lib.ConvertStrToInt(adWidth)),		// required
+		Height: pUint32(lib.ConvertStrToInt(adHeight)),		// required
 	}
-	adType := bd.AdSlot_StaticInfo_BANNER
-	adStaticInfo := bd.AdSlot_StaticInfo{
-		Type: &adType,
-	}
+//	adType := bd.AdSlot_StaticInfo_BANNER
+//	adStaticInfo := bd.AdSlot_StaticInfo{
+//		Type: &adType,
+//	}
 
-	var requestId string = "12341234123413241234";
+	var requestId string = demand.AdRequest.Bid;
 	req := bd.BidRequest{
 		RequestId: &requestId,
-//		IsDebug: &true,
 		ApiVersion: apiVersion,
 		App: app,
 		Device: dev,
@@ -132,35 +160,86 @@ func invokeBD(demand *Demand) {
 			&bd.AdSlot{
 				Id: &adSpaceId,				// required.
 				Size: &adSize,				// required
-				StaticInfo: &adStaticInfo,	// required.
+//				StaticInfo: &adStaticInfo,
 			},
 		},
 	}
 
+	beego.Debug("baidu request: ", req.String())
+
 	data, err := proto.Marshal(&req)
 
-	resp, err := goreq.Request{
-		Method: "POST",
-		Uri:         demand.URL,
-		Timeout: time.Duration(demand.Timeout) * time.Millisecond,
-		Proxy: "http://localhost:8888",
-		Body: data,
-	}.Do()
-
-	bidResp := &bd.BidResponse{}
-	respStr, err := resp.Body.ToString()
-	err = proto.Unmarshal([]byte(respStr), bidResp)
-
 	if err != nil {
-		beego.Debug("error: ", err)
+		generateErrorResp(800, "failed to marshal request", err, demand)
 	} else {
-		beego.Debug("-------- get response from baidu: ", bidResp)
-	}
+		adResponse := new(m.AdResponse)
+		adResponse.Bid = demand.AdRequest.Bid
+		adResponse.SetDemandAdspaceKey(demand.AdspaceKey)
+		adResponse.SetResponseTime(time.Now().Unix())
 
+
+		resp, err := goreq.Request{
+			Method: "POST",
+			Uri:         demand.URL,
+			Timeout: time.Duration(demand.Timeout) * time.Millisecond,
+			Body: data,
+		}.Do()
+
+		if err != nil {
+			generateErrorResp(801, "failed to send request to baidu", err, demand)
+		} else {
+			bidResp := &bd.BidResponse{}
+			respStr, err := resp.Body.ToString()
+
+			if err != nil {
+				generateErrorResp(802, "failed to get response body", err, demand)
+			} else {
+
+				err = proto.Unmarshal([]byte(respStr), bidResp)
+
+				if err != nil {
+					generateErrorResp(803, "failed to unmarshal response body", err, demand)
+				} else {
+					beego.Debug("baidu response: ", bidResp.String())
+					mapBDResponse(bidResp, adResponse)
+					demand.Result <- adResponse
+				}
+			}
+		}
+	}
 }
 
 func pUint32(v int) *uint32 {
 	p := new(uint32)
 	*p = uint32(v)
 	return p
+}
+
+func mapBDResponse(bdResp *bd.BidResponse, adResponse *m.AdResponse) {
+	adResponse.StatusCode = int(*bdResp.ErrorCode)
+	adResponse.SetResponseTime(time.Now().Unix())
+
+	if adResponse.StatusCode == 0 {
+		adUnit := new(m.AdUnit)
+		adResponse.Adunit = adUnit
+		if len(bdResp.GetAds()) > 0 {
+			ad := bdResp.GetAds()[0]
+			adMeta := ad.MaterialMeta
+			adUnit.Cid = *ad.AdslotId
+			adUnit.ClickUrl = *adMeta.ClickUrl
+			//todo hardcode 3 for MH, only support picture ad
+			//adUnit.CreativeType = 3
+			adUnit.ImpTrackingUrls = []string{*adMeta.ShowUrl}
+			// baidu doens't need the tracking url
+//			adUnit.ClkTrackingUrls = nil
+			adUnit.AdWidth = int(*adMeta.MediaWidth)
+			adUnit.AdHeight = int(*adMeta.MediaHeight)
+		}
+	}
+}
+
+func generateErrorResp(errorCode int, message string, err error, demand *Demand) {
+	beego.Critical(err.Error())
+	adResponse := generateErrorResponse(demand.AdRequest, errorCode)
+	demand.Result <- adResponse
 }
